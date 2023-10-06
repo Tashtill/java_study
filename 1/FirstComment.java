db6880da673dc6afe8d43b86a7e51878507e45ee
Mahiro <mhrt2000@gmail.com>
2023-10-06 14:04:29 +0900

    study variable

---
+++ 1/FirstComment.java
@@ -0,0 +1,11 @@
+/*
+ 文字列を表示
+ */
+
+class First{
+    public static void main(String[] args){
+        System.out.println("Hello world!"); //コメントアウト例
+    }
+}
+
+
---
+++ 1/Variable.java
@@ -0,0 +1,10 @@
+class Variable {
+    public static void main(String[] args){
+    int month; //変数宣言
+    month = 10; //値を代入   
+    int day = 6; //初期化（宣言と代入を同時にやる方法）でもOK
+
+    System.out.println("このプログラムは");
+    System.out.println(month + "月" + day + "日に作成されました");
+    }
+}
