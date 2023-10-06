a5a359092be8e50285da8d18fda0605df49e122b
Mahiro <mhrt2000@gmail.com>
2023-10-06 15:37:07 +0900

    study imput from command line

--- 2/Array.java
+++ 2/Array.java
@@ -8,6 +8,11 @@ class Array {
         String[] name = {"カレー", "ピザ", "寿司"}; //初期化を行うパターン
 
         System.out.println(name[0] + "の点数は" + score[0]);
+
+        int[][] scores = {{1,2,3},{3,4,5}}; //int[][]とすれば2次元配列も可能。多次元も同様。
+        System.out.println(scores[0][2]);
+        System.out.println(scores.length);
+        System.out.println(scores[0].length);
     }
     
 }
---
+++ 2/CmdLine.java
@@ -0,0 +1,7 @@
+class CmdLine {
+    public static void main(String[] args){ //mainのカッコ内では文字列を格納できるargsという配列を作成してる
+        System.out.println(args.length);
+        System.out.println(args[1]);
+    }
+    
+}
