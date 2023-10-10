277e421571fa82075dd970d87f8978fddee0757b
Mahiro <mhrt2000@gmail.com>
2023-10-11 00:14:47 +0900

    solved car class practice

---
+++ 3/DriveCar.java
@@ -0,0 +1,11 @@
+class DriveCar {
+    public static void main(String[] args){
+        CarPractice c1 = new CarPractice();
+
+        c1.setNo(2525);
+        c1.run(30);
+        c1.display();
+        c1.stop();
+        c1.display();
+    }
+}
