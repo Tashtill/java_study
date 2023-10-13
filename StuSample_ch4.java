527ce8b0c4d83ad8200d0593ab2f6902d91fe9bd
Mahiro_LP <mhrt2000@gmail.com>
2023-10-13 16:46:36 +0900

    study final

--- StuSample_ch4.java
+++ StuSample_ch4.java
@@ -1,13 +1,16 @@
 public class StuSample_ch4 {
     public static void main(String[] args){
-        Student_ch4 stu = new Student_ch4("菅原");
+        final int BASE_NO = 1000; //他の値を代入できない定数として初期化
+
+        Student_ch4 stu1 = new Student_ch4("菅原", BASE_NO+1);
+        Student_ch4 stu2 = new Student_ch4("宮下", BASE_NO+2);
 
         //スーパークラスのメソッド
         // stu.setName("菅原");
-        stu.display();
+        stu1.display();
 
         //サブクラスのメソッド
-        stu.setStuNo(1);
-        stu.display();
+        //stu2.setStuNo(1);
+        stu2.display();
     }
 }
--- Student_ch4.java
+++ Student_ch4.java
@@ -1,15 +1,10 @@
-public class Student_ch4 extends Person_ch4 {
+public final class Student_ch4 extends Person_ch4 {
     private int stuNo;
 
     public Student_ch4(String name, int stuNo){
         super(name);
         this.stuNo = stuNo;
     }
-
-    public Student_ch4(String name){
-        this(name, 999);
-    }
-
 
     public void setStuNo(int num){
         stuNo = num;
@@ -20,6 +15,6 @@ public class Student_ch4 extends Person_ch4 {
     public void display(){ //Person_ch4にあるdisplayメソッドをオーバーライドした
         super.display(); //スーパークラス中のdisplayメソッドを呼び出す。オーバーライドした場合に同じ処理を書かなくて済む
         System.out.println("学籍番号 : " + stuNo);
-        System.out.println("学籍番号の表示はサブクラスで再定義");
+        //System.out.println("学籍番号の表示はサブクラスで再定義");
     }
 }
